dcc2035d6392010f21af1c6ca2adc57899dae15c
aaron.condori <aaron.condori@tecsup.edu.pe>
2024-10-09 18:13:52 -0500

    Initial Commit

---
+++ CondoriSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CondoriSoundBoard
+//
+//  Created by Aaron Condori on 9/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
